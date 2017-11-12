851869dae1c8120192ac3b0505055f0a6c5d8098
Jim Matheson <jimbmatheson@gmail.com>
2017-11-11 20:55:10 -0500

    UCD-001 - moved cursor tracking into the DisplayAnImage class

--- components/App.js
+++ components/App.js
@@ -3,6 +3,14 @@ import logo from '../img/dragon.jpg'; // relative path to image
 import '../sass/App.scss';
 
 class App extends React.Component{
+	render() {
+	  return (
+	  		<DisplayAnImage />
+	  	);
+	}  
+}
+
+class DisplayAnImage extends React.Component {
 	constructor(props) {
 	    super(props);
 
@@ -11,25 +19,17 @@ class App extends React.Component{
 
 	_onMouseMove(e) {
 	  this.setState({ x: e.screenX, y: e.screenY });
-	}
-
+	}	
+	
 	render() {
-	  const { x, y } = this.state;
-	  return (
-	  	<div onMouseMove={this._onMouseMove.bind(this)}>
-	  		<DisplayAnImage />
-	    	<h1 class='mouse-coords'>Mouse coordinates: x:{ x } | y:{ y }</h1>
-	  	</div>
-	  	);
-	}  
-}
-
-class DisplayAnImage extends React.Component {
-  render() {
-    return ( 
-        <img src={logo} alt={"logo"}/>
-    );
-  }
+	  	const { x, y } = this.state;
+	    return ([ 
+	    	<picture class='p-img-wrapper' onMouseMove={this._onMouseMove.bind(this)}> 
+	        	<img src={logo} alt={"logo"}/>
+	        </picture>,
+	        <div class='mouse-coords'> Mouse coordinates: x:{ x } | y:{ y } </div>
+	    ]);
+	}
 }
 
 export default App
